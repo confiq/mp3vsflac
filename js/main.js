ca112329b8b6f17b0ee446829cf815f57aa9f155
confiq <passwd1>
2012-09-18 18:23:19 +0300

    flac testing. It's working

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+$(document).ready(function(){
+   $('#cp_container_1').click(function(){
+       var player = null;
+      //ok, lets check the status...
+       if (player) player.stop(); //TODO: Change to own method so we can change CSS as well...
+
+       //player = Player.fromFile('./music/file0');
+       player = Player.fromURL('./music/file0.flac');
+       player.on('error', function(e) { console.log(e) });
+
+       console.log(player)
+
+       player.on('metadata', function(data) {
+           console.log(data);
+           var buf = data['Cover Art'];
+           if (!buf) return;
+
+           // Show the album art
+           if (buf)
+               document.querySelector("img").src = buf.toBlobURL();
+       });
+
+       player.play();
+   });
+});
 
